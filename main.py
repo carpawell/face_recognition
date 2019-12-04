e208c3ef44cd3db9d53a6cba9e6748194331d58d
Pavel Karpiy <pasha.karpiy98@gmail.com>
2019-12-04 17:25:40 +0300

    refactor(main.py): add comments

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from signs import *
 from sorting import set_num_of_standards
 
-methods = ["hist", "dft", "dct", "scale", "gradient"]
+methods = ["hist", "dft", "dct", "scale", "gradient"]  # methods for LAB №3
 
-visualize = True
-show_images = True
+visualize = True  # show graphs
+show_images = True  # show images in optimal search
 
-set_num_of_standards(6)
+num_of_standards = 6  # set num of standards(training dataset) before executable cycle
 
+set_num_of_standards(num_of_standards)
+
+#  ======== LAB №3 ==========
 # for method in methods:
 #     search(method, visualize=visualize)
 
+
+#  ======== LAB №4 ==========
 search_optimal(visualize, show_images)
